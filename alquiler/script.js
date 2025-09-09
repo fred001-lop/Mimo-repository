266f7032a9cccf388812f0144fd1d33a98dd1b44
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-09-09 17:01:27 -0600

    add new files

---
+++ alquiler/script.js
@@ -0,0 +1,9 @@
+
+
+function magic(){
+    document.getElementById('rental').innerHTML = "Fredy bike Rental";
+}
+
+function restart(){
+    document.getElementById('rental').innerHTML = "Ben's Bike rental";
+}
